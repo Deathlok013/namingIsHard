86d92bd327e5cfc339e994a494c7425092ff2231
Alexandr <petrov.alex23@proton.me>
2024-05-25 20:52:06 +0300

    Initial Commit

---
+++ namingIsHard/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  namingIsHard
+//
+//  Created by Macbook on 25.05.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
